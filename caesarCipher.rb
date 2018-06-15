e08878504b8557d7c0ae1e56eaf2d1aa8e4de7de
w-wakayama <tsumamist715@gmail.com>
2018-06-15 14:28:28 +0900

    空文字を判定

--- caesarCipher.rb
+++ caesarCipher.rb
@@ -1,4 +1,4 @@
-str = "RUBY"
+str = "RUBY IS GOOD"
 rot = 3
 
 ans = ""
@@ -7,6 +7,10 @@ arr = ("A".."Z").to_a #AからZまでの文字配列
 strs = str.chars    #文字列を一文字区切りにした配列
 
 strs.each{|s|
+  if s == " " then
+    ans += s
+    next
+  end
   i = arr.find_index{|st| st == s}
   #Zより先になってしまった場合の処理
   if (i+rot) >= 26 then
