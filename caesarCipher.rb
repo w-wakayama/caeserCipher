5b56ba1958f66e9ac7ca38579f8399ce703bf49b
w-wakayama <tsumamist715@gmail.com>
2018-06-15 15:28:19 +0900

    大文字小文字判定できた

--- caesarCipher.rb
+++ caesarCipher.rb
@@ -1,16 +1,14 @@
-str = "RUBY IS GOOD"
+str = "Ruby Is Good"
 rot = 3
 
 ans = ""
 
 arr = ("A".."Z").to_a #AからZまでの文字配列
+arr2 = ("a".."z").to_a
+
 strs = str.chars    #文字列を一文字区切りにした配列
 
-strs.each{|s|
-  if s == " " then
-    ans += s
-    next
-  end
+def caesarCipher(s,rot,arr)
   i = arr.find_index{|st| st == s}
   #Zより先になってしまった場合の処理
   if (i+rot) >= 26 then
@@ -18,7 +16,20 @@ strs.each{|s|
   else
     n = i+rot
   end
-  ans += arr[n]
+  return arr[n]
+end
+
+strs.each{|s|
+  if s == " " then
+    ans += s
+    next
+  end
+
+  if s == s.upcase then
+    ans += caesarCipher(s,rot,arr)
+  else
+    ans += caesarCipher(s,rot,arr2)
+  end
 }
 
 p(ans)
